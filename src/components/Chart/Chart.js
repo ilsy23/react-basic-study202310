c542542b4b3a7c88d496d700e360e0704231d0c8
ilsy23 <ilsysli@naver.com>
2023-11-02 16:09:50 +0900

    23. 11. 02. 수업

---
+++ src/components/Chart/Chart.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+import ChartBar from './ChartBar';
+import './Chart.css';
+
+const Chart = ({ dataPoints }) => {
+  // 1년치 지출 총액 필요 (그래야 비율 설정 가능)
+  const dataPointValues = dataPoints.map((dp) => dp.value);
+
+  // 1년치 총액
+  // a: 리턴결과에 대한 누적값, b: 배열에서 하나씩 꺼낸 값
+  const totalValue = dataPointValues.reduce((a, b) => a + b, 0); // 0: 초기값
+
+  // 그 중에서 제일 지출이 높은 값
+  //   const maximumValue = Math.max(...dataPointValues);
+  //   console.log('maximum: ', maximumValue);
+
+  return (
+    <div className='chart'>
+      {dataPoints.map(({ label, value }) => {
+        return (
+          <ChartBar
+            key={label}
+            label={label}
+            currentValue={value}
+            maxValue={totalValue}
+          />
+        );
+      })}
+    </div>
+  );
+};
+
+export default Chart;
